e15599f97b2750f532a380d4719d2fe5f98e3002
Tanishq Rachamalla <104064377+Tanishq251@users.noreply.github.com>
2024-07-02 20:21:53 +0530

    Time: 7 ms (87.26%) | Memory: 13.5 MB (54.81%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int> arr(1001, 0);
-        for (auto x : trips)
-        {
-            arr[x[1]] += x[0];
-            arr[x[2]] -= x[0];
+        map<int, int> timestamp;
+        for (const auto& trip : trips) {
+            timestamp[trip[1]] += trip[0];
+            timestamp[trip[2]] -= trip[0];
         }
-        for (int i = 0; i < arr.size(); i++)
-        {
-            capacity -= arr[i];
+        for (const auto& entry : timestamp) {
+            capacity -= entry.second;
             if (capacity < 0)
                 return false;
         }
