cdf43db71fa740408585a1870ebbb49597548f2c
Tanishq Rachamalla <104064377+Tanishq251@users.noreply.github.com>
2024-07-02 20:04:33 +0530

    Time: 3 ms (71.47%) | Memory: 10.8 MB (22.07%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        int arr[2051];
-        for(int i = 0; i< logs.size(); i++){
-            for(int j = logs[i][0]; j < logs[i][1]; j++){
-                arr[j]++;
-            }
-        }
-        int max=0, year=0;
-        for(int i = 0; i <2051;i++){
-            if(arr[i]>max){
-                max = arr[i];
-                year= i;
-            }
-        }
-        return year;
+        int population[2051] = {0};
 
+        for(const auto& log: logs){
+            population[log[0]]++;
+            population[log[1]]--;
+        }
+        int maxPopulation= 0;
+        int maxYear = 1950;
+        int currentPopulation = 0;
+
+        for(int year = 1950; year< 2051; year++){
+            currentPopulation+=population[year];
+            if(currentPopulation>maxPopulation){
+                maxPopulation = currentPopulation;
+                maxYear= year;
+            }
+        }
+        return maxYear;
     }
 };
