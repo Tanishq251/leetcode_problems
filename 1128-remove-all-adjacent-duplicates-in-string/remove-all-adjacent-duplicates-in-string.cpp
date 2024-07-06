477445c6ab20204fff0e297bffca70426e52bcd7
Tanishq Rachamalla <104064377+Tanishq251@users.noreply.github.com>
2024-07-06 10:39:42 +0530

    Time: 25 ms (35.05%) | Memory: 12.8 MB (38.51%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            if (st.empty() || st.top() != s[i]) {
+                st.push(s[i]);
+            } else {
+                st.pop();
+            }
+        }
+        string ans = "";
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop(); 
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
