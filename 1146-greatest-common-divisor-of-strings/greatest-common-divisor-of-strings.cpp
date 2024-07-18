0d53a1e6625123cc9aa696c2480e2dfd364d9521
Tanishq Rachamalla <104064377+Tanishq251@users.noreply.github.com>
2024-07-18 19:48:25 +0530

    Time: 6 ms (25.31%) | Memory: 10.4 MB (13.03%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
         if (str1.length() < str2.length()) 
-            return gcdOfStrings(str2, str1); // Ensure str1 is always the longer one
+            return gcdOfStrings(str2, str1); 
         
         if (str1.find(str2) != 0) 
-            return ""; // If str1 does not start with str2, there is no common divisor
+            return ""; 
         
         if (str2 == "") 
-            return str1; // If str2 is empty, return str1 as the GCD
+            return str1;
         
-        // Recur with the substring of str1 from length of str2 to the end
         return gcdOfStrings(str1.substr(str2.length()), str2);
     }
 };
+
