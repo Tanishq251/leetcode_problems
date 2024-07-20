05d715f59ba030d53725ea50d36d247e7e111446
Tanishq Rachamalla <104064377+Tanishq251@users.noreply.github.com>
2024-07-20 08:12:22 +0530

    Time: 19 ms (44.93%) | Memory: 34.5 MB (29.50%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0, left =0;
+        for(int i: nums) total+= i;
+
+        for(int i=0;i<nums.size();i++){
+            if(left == total - left - nums[i]) return i;
+            left+=nums[i];
+        }
+
+        return -1;
+    }
+};
