26dd01b98d70cd1b099efac3ae6b5a1c0ce39ed8
Tanishq Rachamalla <104064377+Tanishq251@users.noreply.github.com>
2024-08-09 12:22:18 +0530

    Time: 279 ms (74.77%) | Memory: 146.1 MB (27.02%) - LeetSync

--- 2104-operations-on-tree/operations-on-tree.cpp
+++ 2104-operations-on-tree/operations-on-tree.cpp
@@ -2,64 +2,70 @@ class LockingTree {
 private:
     vector<int> parent;
     vector<int> locked;
-    vector<vector<int>> children;
+    vector<unordered_set<int>> children;
+    vector<int> lockedDescendantsCount;
 
 public:
     LockingTree(vector<int>& parent) : parent(parent) {
         int n = parent.size();
         locked.resize(n, -1);
         children.resize(n);
+        lockedDescendantsCount.resize(n, 0);
         for (int i = 1; i < n; ++i) {
-            children[parent[i]].push_back(i);
+            children[parent[i]].insert(i);
         }
     }
     
     bool lock(int num, int user) {
         if (locked[num] != -1) return false;
         locked[num] = user;
+        updateAncestors(num, 1);
         return true;
     }
     
     bool unlock(int num, int user) {
         if (locked[num] != user) return false;
         locked[num] = -1;
+        updateAncestors(num, -1);
         return true;
     }
     
     bool upgrade(int num, int user) {
-        if (locked[num] != -1) return false;
+        if (locked[num] != -1 || lockedDescendantsCount[num] == 0) return false;
         
         // Check if the node has any locked ancestors
-        int cur = num;
+        int cur = parent[num];
         while (cur != -1) {
             if (locked[cur] != -1) return false;
             cur = parent[cur];
         }
         
-        // Check if the node has any locked descendants
-        vector<int> lockedDescendants;
-        dfs(num, lockedDescendants);
-        
-        if (lockedDescendants.empty()) return false;
-        
         // Unlock all descendants
-        for (int desc : lockedDescendants) {
-            locked[desc] = -1;
-        }
+        unlockDescendants(num);
         
         // Lock the current node
         locked[num] = user;
+        updateAncestors(num, 1);
         
         return true;
     }
     
 private:
-    void dfs(int node, vector<int>& lockedDescendants) {
+    void updateAncestors(int node, int delta) {
+        int cur = parent[node];
+        while (cur != -1) {
+            lockedDescendantsCount[cur] += delta;
+            cur = parent[cur];
+        }
+    }
+    
+    void unlockDescendants(int node) {
         if (locked[node] != -1) {
-            lockedDescendants.push_back(node);
+            locked[node] = -1;
+            updateAncestors(node, -1);
         }
         for (int child : children[node]) {
-            dfs(child, lockedDescendants);
+            unlockDescendants(child);
         }
     }
 };
