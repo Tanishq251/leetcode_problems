f39436586c2ab68a53ee3bc02d86e36b0c930443
Tanishq Rachamalla <104064377+Tanishq251@users.noreply.github.com>
2024-08-09 12:27:10 +0530

    Time: 644 ms (8.10%) | Memory: 169.7 MB (24.77%) - LeetSync

--- 2104-operations-on-tree/operations-on-tree.cpp
+++ 2104-operations-on-tree/operations-on-tree.cpp
@@ -1,43 +1,43 @@
 class LockingTree {
 private:
-    vector<int> parent;
-    vector<int> locked;
-    vector<unordered_set<int>> children;
-    vector<int> lockedDescendantsCount;
+    unordered_map<int, int> parent;
+    unordered_map<int, int> locked;
+    unordered_map<int, std::unordered_set<int>> children;
+    unordered_map<int, int> lockedDescendantsCount;
 
 public:
-    LockingTree(vector<int>& parent) : parent(parent) {
+    LockingTree(vector<int>& parent) {
         int n = parent.size();
-        locked.resize(n, -1);
-        children.resize(n);
-        lockedDescendantsCount.resize(n, 0);
-        for (int i = 1; i < n; ++i) {
-            children[parent[i]].insert(i);
+        for (int i = 0; i < n; ++i) {
+            this->parent[i] = parent[i];
+            if (i > 0) {
+                children[parent[i]].insert(i);
+            }
         }
     }
     
     bool lock(int num, int user) {
-        if (locked[num] != -1) return false;
+        if (locked.count(num)) return false;
         locked[num] = user;
         updateAncestors(num, 1);
         return true;
     }
     
     bool unlock(int num, int user) {
-        if (locked[num] != user) return false;
-        locked[num] = -1;
+        if (!locked.count(num) || locked[num] != user) return false;
+        locked.erase(num);
         updateAncestors(num, -1);
         return true;
     }
     
     bool upgrade(int num, int user) {
-        if (locked[num] != -1 || lockedDescendantsCount[num] == 0) return false;
+        if (locked.count(num) || lockedDescendantsCount[num] == 0) return false;
         
         // Check if the node has any locked ancestors
-        int cur = parent[num];
-        while (cur != -1) {
-            if (locked[cur] != -1) return false;
+        int cur = num;
+        while (parent.count(cur)) {
             cur = parent[cur];
+            if (locked.count(cur)) return false;
         }
         
         // Unlock all descendants
@@ -52,16 +52,15 @@ public:
     
 private:
     void updateAncestors(int node, int delta) {
-        int cur = parent[node];
-        while (cur != -1) {
-            lockedDescendantsCount[cur] += delta;
-            cur = parent[cur];
+        while (parent.count(node)) {
+            node = parent[node];
+            lockedDescendantsCount[node] += delta;
         }
     }
     
     void unlockDescendants(int node) {
-        if (locked[node] != -1) {
-            locked[node] = -1;
+        if (locked.count(node)) {
+            locked.erase(node);
             updateAncestors(node, -1);
         }
         for (int child : children[node]) {
