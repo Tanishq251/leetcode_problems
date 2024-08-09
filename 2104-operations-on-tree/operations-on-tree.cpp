bf9a3584ca2489a1090c726840d6e29ae35018a1
Tanishq Rachamalla <104064377+Tanishq251@users.noreply.github.com>
2024-08-09 19:31:36 +0530

    Time: 239 ms (96.85%) | Memory: 125.1 MB (99.55%) - LeetSync

--- 2104-operations-on-tree/operations-on-tree.cpp
+++ 2104-operations-on-tree/operations-on-tree.cpp
@@ -1,105 +1,147 @@
-struct MyTreeNode {
-    int val;
-    int lockedBy; // Stores the user who locked the node, or -1 if unlocked
-    MyTreeNode* parent; // Pointer to the parent node
-    vector<MyTreeNode*> children; // List of child nodes
-    int lockedDescendantCount; // Count of locked descendants
 
-    MyTreeNode(int v, MyTreeNode* p = nullptr) : val(v), lockedBy(-1), parent(p), lockedDescendantCount(0) {}
-};
 
 class LockingTree {
 private:
-    unordered_map<int, MyTreeNode*> nodeMap; // Mapping from node value to MyTreeNode pointer
-    unordered_map<int, int> lockedNodes; // Mapping from locked node to the user
+    struct MyTreeNode {
+        int val;
+        int lockedBy;
+        int parent;
+        int childrenStart;
+        int childrenCount;
+        int lockedDescendantCount;
+        int depth;
+        
+        MyTreeNode(int v, int p = -1) : val(v), lockedBy(-1), parent(p), 
+                                        childrenStart(-1), childrenCount(0),
+                                        lockedDescendantCount(0), depth(0) {}
+    };
+    
+    std::vector<MyTreeNode> nodes;
+    std::vector<int> allChildren;
+    std::unordered_set<int> lockedNodes;
 
 public:
-    LockingTree(vector<int>& parent) {
+    LockingTree(std::vector<int>& parent) {
         int n = parent.size();
+        nodes.reserve(n);
         for (int i = 0; i < n; ++i) {
-            nodeMap[i] = new MyTreeNode(i);
+            nodes.emplace_back(i, parent[i]);
         }
-
-        // Building the tree
+        
+        std::vector<int> childrenCounts(n, 0);
         for (int i = 1; i < n; ++i) {
-            int p = parent[i];
-            nodeMap[i]->parent = nodeMap[p];
-            nodeMap[p]->children.push_back(nodeMap[i]);
+            if (parent[i] >= 0 && parent[i] < n) {
+                childrenCounts[parent[i]]++;
+            }
+        }
+        
+        int totalChildren = 0;
+        for (int i = 0; i < n; ++i) {
+            nodes[i].childrenStart = totalChildren;
+            totalChildren += childrenCounts[i];
+        }
+        
+        allChildren.resize(totalChildren);
+        std::vector<int> nextChildIndex(n);
+        for (int i = 0; i < n; ++i) {
+            nextChildIndex[i] = nodes[i].childrenStart;
+        }
+        
+        for (int i = 1; i < n; ++i) {
+            if (parent[i] >= 0 && parent[i] < n) {
+                int p = parent[i];
+                allChildren[nextChildIndex[p]++] = i;
+                nodes[p].childrenCount++;
+            }
+        }
+        
+        calculateDepth(0, 0);
+    }
+
+    void calculateDepth(int nodeIndex, int depth) {
+        nodes[nodeIndex].depth = depth;
+        int childrenEnd = nodes[nodeIndex].childrenStart + nodes[nodeIndex].childrenCount;
+        for (int i = nodes[nodeIndex].childrenStart; i < childrenEnd; ++i) {
+            calculateDepth(allChildren[i], depth + 1);
         }
     }
 
     bool lock(int num, int user) {
-        MyTreeNode* node = nodeMap[num];
-        if (node->lockedBy == -1) { // If the node is unlocked
-            node->lockedBy = user;
-            lockedNodes[num] = user;
-            updateLockedDescendantCount(node, 1);
+        if (nodes[num].lockedBy == -1) {
+            nodes[num].lockedBy = user;
+            lockedNodes.insert(num);
+            int parent = nodes[num].parent;
+            while (parent != -1) {
+                nodes[parent].lockedDescendantCount++;
+                parent = nodes[parent].parent;
+            }
             return true;
         }
-        return false; // Node is already locked
+        return false;
     }
 
     bool unlock(int num, int user) {
-        MyTreeNode* node = nodeMap[num];
-        if (node->lockedBy == user) { // If the node is locked by the same user
-            node->lockedBy = -1;
+        if (nodes[num].lockedBy == user) {
+            nodes[num].lockedBy = -1;
             lockedNodes.erase(num);
-            updateLockedDescendantCount(node, -1);
+            int parent = nodes[num].parent;
+            while (parent != -1) {
+                nodes[parent].lockedDescendantCount--;
+                parent = nodes[parent].parent;
+            }
             return true;
         }
-        return false; // Node is either unlocked or locked by a different user
+        return false;
     }
 
     bool upgrade(int num, int user) {
-        MyTreeNode* node = nodeMap[num];
-        if (node->lockedBy != -1 || node->lockedDescendantCount == 0) {
-            return false; // The node is either locked or has no locked descendants
+        if (nodes[num].lockedBy != -1 || nodes[num].lockedDescendantCount == 0) {
+            return false;
         }
-
-        if (hasLockedAncestor(node)) {
-            return false; // Any ancestor is locked
+        
+        int parent = nodes[num].parent;
+        while (parent != -1) {
+            if (nodes[parent].lockedBy != -1) return false;
+            parent = nodes[parent].parent;
         }
-
-        // Unlock all locked descendants in O(log n) by iterating over the lockedNodes map
-        vector<int> toUnlock;
-        for (auto& [lockedNodeNum, _] : lockedNodes) {
-            MyTreeNode* lockedNode = nodeMap[lockedNodeNum];
-            if (isDescendant(node, lockedNode)) {
-                toUnlock.push_back(lockedNodeNum);
+        
+        std::vector<int> toUnlock;
+        for (int locked : lockedNodes) {
+            if (isDescendant(num, locked)) {
+                toUnlock.push_back(locked);
             }
         }
-
-        for (int nodeNum : toUnlock) {
-            unlock(nodeNum, lockedNodes[nodeNum]);
+        
+        for (int locked : toUnlock) {
+            nodes[locked].lockedBy = -1;
+            lockedNodes.erase(locked);
+            parent = nodes[locked].parent;
+            while (parent != -1) {
+                nodes[parent].lockedDescendantCount--;
+                parent = nodes[parent].parent;
+            }
         }
-
-        // Lock the current node
-        return lock(num, user);
+        
+        nodes[num].lockedBy = user;
+        lockedNodes.insert(num);
+        parent = nodes[num].parent;
+        while (parent != -1) {
+            nodes[parent].lockedDescendantCount++;
+            parent = nodes[parent].parent;
+        }
+        return true;
     }
 
 private:
-    bool isDescendant(MyTreeNode* ancestor, MyTreeNode* node) {
-        while (node) {
-            if (node == ancestor) return true;
-            node = node->parent;
-        }
-        return false;
+    bool isDescendant(int ancestor, int descendant) {
+        return nodes[ancestor].depth < nodes[descendant].depth &&
+               isAncestor(ancestor, descendant);
     }
-
-    void updateLockedDescendantCount(MyTreeNode* node, int delta) {
-        MyTreeNode* parent = node->parent;
-        while (parent) {
-            parent->lockedDescendantCount += delta;
-            parent = parent->parent;
+    
+    bool isAncestor(int ancestor, int descendant) {
+        while (nodes[descendant].depth > nodes[ancestor].depth) {
+            descendant = nodes[descendant].parent;
         }
-    }
-
-    bool hasLockedAncestor(MyTreeNode* node) {
-        MyTreeNode* parent = node->parent;
-        while (parent) {
-            if (parent->lockedBy != -1) return true;
-            parent = parent->parent;
-        }
-        return false;
+        return ancestor == descendant;
     }
 };
