570f51388282f343564e6889d58f0c946db6e6ba
Tanishq Rachamalla <104064377+Tanishq251@users.noreply.github.com>
2024-08-15 16:26:34 +0530

    Time: 4 ms (7.83%) | Memory: 8.6 MB (30.83%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,42 +1,41 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> result;
-        if (matrix.empty()) return result;
-        
-        int top = 0, bottom = matrix.size() - 1;
-        int left = 0, right = matrix[0].size() - 1;
-        
-        while (top <= bottom && left <= right) {
-            // Traverse from left to right along the top row
-            for (int i = left; i <= right; i++) {
-                result.push_back(matrix[top][i]);
-            }
-            top++; // Move the top boundary down
+        vector<int> res;
+
+        if(matrix.empty()) return res;
+
+        int top = 0, bottom = matrix.size()-1;
+        int left = 0, right = matrix[0].size()-1;
+
+        while(top <= bottom && left <= right){
             
-            // Traverse from top to bottom along the right column
-            for (int i = top; i <= bottom; i++) {
-                result.push_back(matrix[i][right]);
+            for(int i=left; i<=right; i++){
+                res.push_back(matrix[top][i]);
             }
-            right--; // Move the right boundary left
-            
-            if (top <= bottom) {
-                // Traverse from right to left along the bottom row
-                for (int i = right; i >= left; i--) {
-                    result.push_back(matrix[bottom][i]);
+            top++;
+
+            for(int i=top; i<=bottom; i++){
+                res.push_back(matrix[i][right]);
+            }
+            right--;
+
+            if(top <= bottom){
+
+                for(int i=right; i>=left; i--){
+                    res.push_back(matrix[bottom][i]);
                 }
-                bottom--; // Move the bottom boundary up
+                bottom--;
             }
-            
-            if (left <= right) {
-                // Traverse from bottom to top along the left column
-                for (int i = bottom; i >= top; i--) {
-                    result.push_back(matrix[i][left]);
+
+            if(left <= right){
+                for(int i=bottom; i>=top; i--){
+                    res.push_back(matrix[i][left]);
                 }
-                left++; // Move the left boundary right
+                left++;
             }
+
         }
-        
-        return result;
+        return res;
     }
 };
