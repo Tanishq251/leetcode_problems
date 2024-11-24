38347c0d9c211260d2bb1240ec21f0caf6d01a8e
Tanishq Rachamalla <104064377+Tanishq251@users.noreply.github.com>
2024-11-24 06:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (68.04%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if (str1.length() < str2.length()) 
-            return gcdOfStrings(str2, str1); 
+        if (str1 + str2 != str2 + str1) {
+            return "";
+        }
         
-        if (str1.find(str2) != 0) 
-            return ""; 
-        
-        if (str2 == "") 
-            return str1;
-        
-        return gcdOfStrings(str1.substr(str2.length()), str2);
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substr(0, gcdLength);
     }
 };
-
